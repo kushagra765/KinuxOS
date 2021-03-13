889b6437588031ab071dd601c8c1385812ebff5d
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-13 18:15:26 +0530

    Update string.h

--- include/string.h
+++ include/string.h
@@ -1,7 +1,11 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-#include <stddef.h>
+/* include/string.h
+ * Author - @kushagra765
+ */
+
+#include "stddef.h"
 
 int strcmp(const char *str1, const char *str2);
 char *strcpy(char *str1, char *str2);
