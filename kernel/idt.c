082da8db76f31fff516b10d8ecff77b660381669
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-13 15:05:46 +0530

    Add files via upload

---
+++ kernel/idt.c
@@ -0,0 +1,41 @@
+#include "../include/stdint.h"
+#include "../include/string.h"
+#include "idt.h"
+
+struct entry_idt {
+   uint16_t lo_base;
+   uint16_t sel;
+   uint8_t always0;
+   uint8_t flags;
+   uint16_t hi_base;
+} __attribute__((packed));
+
+
+struct pointer_idt {
+   uint16_t limit;
+   uint32_t base;
+} __attribute__((packed));
+
+
+struct entry_idt idt[256];
+struct pointer_idt idt_ptr;
+
+void set_gate_idt(int num, uint32_t base, uint16_t sel, uint8_t flags) {
+     
+     idt[num].lo_base = base & 0xFFFF;
+     idt[num].hi_base = (base >> 16) & 0xFFFF;
+     idt[num].sel = sel;
+     idt[num].always0 = 0;
+     idt[num].flags = flags;
+}
+
+void init_idt() {
+     
+     idt_ptr.limit = (sizeof(struct entry_idt)*256) - 1;
+     idt_ptr.base = (uintptr_t)&idt;
+     memset(&idt, 0, sizeof(struct entry_idt)*256);
+     
+     idt_load();
+    
+}
+
---
+++ kernel/terminal.c
@@ -0,0 +1,6 @@
+#include "../include/kinux/terminal.h"
+#include "../drivers/vga/vga.h"
+
+void terminal() {
+    putstr("user@Kinux: \n", COLOR_WHT, COLOR_LBU);
+}
