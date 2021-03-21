29f336eb59ede91cb2ec5b2745f00c9f4152b135
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-21 16:55:59 +0530

    Updated build_compiler.sh

--- compiler/build_compiler.sh
+++ compiler/build_compiler.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -d "./toolchain" ]; then
+if [ -d "toolchain/" ]; then
     echo "Found Toolchain"
     exit 0;
 else
@@ -10,7 +10,7 @@ fi
 sudo apt-get update
 sudo apt install nasm make gcc bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev tar xorriso mtools
 
-export PREFIX="./toolchain"
+export PREFIX="/toolchain"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 
@@ -19,8 +19,17 @@ mkdir ./toolchain
 mkdir ./src
 cd ./src
 
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.36.tar.gz
-wget https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
+if [ -e binutils-2.36.tar.gz ]; then
+   echo "Found binutils archive"
+else
+   wget https://ftp.gnu.org/gnu/binutils/binutils-2.36.tar.gz
+fi
+
+if [ -e gcc-10.2.0.tar.gz ]; then
+   echo "Found gcc archive"
+else
+   wget https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
+fi
 
 tar -xf gcc-10.2.0.tar.gz
 tar -xf binutils-2.36.tar.gz
@@ -31,7 +40,7 @@ cd binutils-build
 make
 make install
 
-cd ./src
+cd ..
 
 which -- $TARGET-as || echo $TARGET-as not found
  
