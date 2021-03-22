79f1162ac824d2207c3608d2454466ad7e805f96
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-22 17:12:19 +0530

    Update build_libc.sh

--- build_libc.sh
+++ build_libc.sh
@@ -9,9 +9,11 @@ mkdir libc-build
 cd libc-build
 
 # Build ctype/
-i686-elf-gcc -Wall -Wextra -O2 -c ../libc/ctype/isalnum.c -o ../libc-build/isalnum.o
-i686-elf-gcc -Wall -Wextra -O2 -c ../libc/ctype/isalpha.c -o ../libc-build/isalpha.o
-i686-elf-gcc -Wall -Wextra -O2 -c ../libc/ctype/isblank.c -o ../libc-build/isblank.o
+i686-elf-gcc -Wall -Wextra -O2 -I./include/ -c ../libc/ctype/isalnum.c -o ../libc-build/isalnum.o
+i686-elf-gcc -Wall -Wextra -O2 -I./include/ -c ../libc/ctype/isalpha.c -o ../libc-build/isalpha.o
+i686-elf-gcc -Wall -Wextra -O2 -I./include/ -c ../libc/ctype/isblank.c -o ../libc-build/isblank.o
+i686-elf-gcc -Wall -Wextra -O2 -I./include/ -c ../libc/ctype/isdigit.c -o ../libc-build/isdigit.o
+i686-elf-gcc -Wall -Wextra -O2 -I./include/ -c ../libc/ctype/islower.c -o ../libc-build/islower.o
 
 # Build errno/
 i686-elf-gcc -Wall -Wextra -O2 -c ../libc/errno/errno.c -o ../libc-build/errno.o
