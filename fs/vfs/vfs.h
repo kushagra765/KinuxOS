32db1bc62333daa6376fd4901e4a4e90a904f40c
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-23 13:36:02 +0530

    Update vfs.h

--- fs/vfs/vfs.h
+++ fs/vfs/vfs.h
@@ -7,13 +7,24 @@
 
 #include <stdint.h>
 
-struct node_vfs {
+typedef struct node_vfs {
    char fname[50];
    uint32_t uid;
    uint32_t gid;
    uint32_t file_len;
    uint32_t flags;
    uint32_t permission;
+   read_vfs_t read;
+} node_vfs_t;
+
+struct dirent_vfs {
+   char fname[50];
+   uint32_t inode_num;
 };
+
+typedef uint32_t (*read_vfs_t)(struct node_vfs*, uint32_t, uint32_t, uint8_t);
+uint32_t read_vfs(node_vfs_t *node, uint32_t offset, uint32_t f_size, uint8_t *buf);
+
+node_vfs_t *vfs_root;
 
 #endif //_VFS_H
