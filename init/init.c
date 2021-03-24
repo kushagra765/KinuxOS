ee606370c2a9678bfb011feab2ea0b9a70e48e00
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-24 20:29:01 +0530

    Init : Use Default Background

--- init/init.c
+++ init/init.c
@@ -12,20 +12,20 @@
 #include <terminal.h>
 
 void init() {
-     clearwin(COLOR_WHT, COLOR_WHT);
-     putstr("[KERNEL] Loading Kernel\n", COLOR_BLK, COLOR_WHT); 
-     putstr("[KINUX] Loading the GDT...\n", COLOR_BLK, COLOR_WHT);
+     putstr("[KERNEL] Loading Kernel\n", COLOR_WHT, COLOR_BLK); 
+     putstr("[KINUX] Loading the GDT...\n", COLOR_WHT, COLOR_BLK);
      init_gdt();
-     putstr("[KINUX] Loading the IDT...\n", COLOR_BLK, COLOR_WHT);
+     putstr("[KINUX] Loading the IDT...\n", COLOR_WHT, COLOR_BLK);
      init_idt();
-     putstr("[KINUX] Loading the ISR...\n", COLOR_BLK, COLOR_WHT);
+     putstr("[KINUX] Loading the ISR...\n", COLOR_WHT, COLOR_BLK);
      init_isr();
-     putstr("[KINUX] Loading the IRQ...\n", COLOR_BLK, COLOR_WHT);
+     putstr("[KINUX] Loading the IRQ...\n", COLOR_WHT, COLOR_BLK);
      init_irq();
-     putstr("[KINUX] Loading Drivers...\n", COLOR_BLK, COLOR_WHT);
+     putstr("[KINUX] Loading Drivers...\n", COLOR_WHT, COLOR_BLK);
      init_timer(50);
      init_keyboard();
      __asm__ volatile("sti");
-     putstr("System Loaded\n", COLOR_GRN, COLOR_WHT);
+     putstr("System Loaded\n", COLOR_GRN, COLOR_BLK);
+     printm("KERNEL LOADED\n");
      terminal();
 }
