ba7e5da69eeba799fab44131ca8778ddaf81f638
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-03-30 15:29:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+sudo apt install nasm mtools
+
 export TARGET=i686-elf
 
 # Change the PATH to where you have installed the i686-elf-gcc compiler
-export PATH="$HOME/cross/bin:$PATH"
+export PATH="./compiler/toolchain/bin:$PATH"
 
 # Build KinuxOS
 mkdir build
@@ -30,7 +32,7 @@ i686-elf-gcc -Wall -Wextra -O2 -I./drivers/vga/ -I./include/ -c ./kernel/idt.c -
 i686-elf-gcc -Wall -Wextra -O2 -I./drivers/vga/ -I./drivers/vga/ -I./include/ -c ./kernel/isr.c -o ./build/isr.o
 i686-elf-gcc -Wall -Wextra -O2 -I./drivers/vga/ -I./include/ -I./include/kinux/ -c ./kernel/irq.c -o ./build/irq.o
 i686-elf-gcc -Wall -Wextra -O2 -I./include/ -I./include/kinux/ -I./drivers/vga/ -I./bin/version/ -I./bin/uname/ -c ./kernel/terminal.c -o ./build/terminal.o
-i686-elf-gcc -O2 -I./include/ -I./include/kinux/ -I./drivers/vga/ -c ./kernel/log.c -o ./build/log.o
+i686-elf-gcc -Wall -Wextra -O2 -I./drivers/vga/ -I./include/ -I./include/kinux/ -c ./kernel/log.c -o ./build/log.o
 
 # Link KinuxOS
 i686-elf-gcc -T linker.ld -o kinuxOS.bin -O2 -nostdlib ./build/boot.o ./build/version.o ./build/uname.o ./build/vga.o ./build/ports.o ./build/keyboard.o ./build/timer.o ./build/init.o ./build/gdt.o ./build/load_gdt.o ./build/idt.o ./build/load_idt.o ./build/isr.o ./build/load_isr.o ./build/irq.o ./build/load_irq.o ./build/terminal.o ./build/log.o
